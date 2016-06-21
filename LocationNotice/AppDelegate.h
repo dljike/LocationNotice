22cbd24141c57e87c05adc7ff7d34d16c9c54de0
dubo <905517804@qq.com>
2016-06-21 11:01:24 +0800

    Initial Commit

---
+++ LocationNotice/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LocationNotice
+//
+//  Created by mac on 16/6/21.
+//  Copyright © 2016年 dubo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LocationNotice/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LocationNotice
+//
+//  Created by mac on 16/6/21.
+//  Copyright © 2016年 dubo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
